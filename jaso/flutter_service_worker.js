92a24dafc9f4c47171c8e8aa3f234fbdaaff66cf
leesy <leesy@rsupport.com>
2022-04-10 18:49:49 +0900

    [v1.0.3] 종성만 보기 option 추가

--- jaso/flutter_service_worker.js
+++ jaso/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "b5995330d2a622a68a6902c4dc2eeecd",
-"index.html": "84467e6296fa2c6248c79699a12707f5",
-"/": "84467e6296fa2c6248c79699a12707f5",
-"main.dart.js": "0c8903d746f642e60c67d228f7fd4b1e",
+  "version.json": "e6f63712130ab5fd24efb5c3dd2c3808",
+"index.html": "68647deb1e4d65fe1a7c027d69a10f48",
+"/": "68647deb1e4d65fe1a7c027d69a10f48",
+"main.dart.js": "9af2a4d4e577b313d1f57a41e5d6ac49",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "39a6adf50aeb70517b7a3344563d5266",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "5e7feb15b4ea57eab8ff9bc6da0322de",
+"assets/NOTICES": "4e8e69f0ee1138c7bddfb3e20c96b42d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
